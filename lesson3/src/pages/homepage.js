0de91d1d47fd58f0e7995db70e9dbcb00827fc29
hoangnamlee04 <150770315+hoangnamlee04@users.noreply.github.com>
2024-01-16 11:14:49 +0700

    f

--- lesson3/src/pages/homepage.js
+++ lesson3/src/pages/homepage.js
@@ -2,19 +2,21 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import Card from "../components/card"
 import Advertisement from "../components/advertisement"
+
+// Data
+import { books } from '../../db.json' assert {type: 'json'}
 // Pages
 const HomePage = function () {
-    return `
+  console.log(books);
+  return `
       ${Header()}
-      <h1>Trang chủ</h1>
-      <div style="display: flex; width: 100%">
-        <div style="width: 40%">${Advertisement()}</div>
-        <div style="width: 60%">
-          ${Card()}
-          ${Card()}
-          ${Card()}
-        </div>
-      </div>
+      <h1 class="text-orange-600">Trang chủ</h1>
+      ${books.map(function (book) {
+    return `
+          <img width="150px" src="${book.images[0].base_url}"/>
+          <a href="">${book.name}</a>
+        `
+  })}
       ${Footer()}
     `
 }
