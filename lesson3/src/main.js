2d3c4bc38752a24f7fac0bbe34e004bcb1bd3bc6
hoangnamlee04 <150770315+hoangnamlee04@users.noreply.github.com>
2024-01-27 10:48:36 +0700

    Buổi 11

--- lesson3/src/main.js
+++ lesson3/src/main.js
@@ -3,6 +3,7 @@ import { books } from "../db.json" assert {type:'json'};
 import HomePage from "./pages/homePages";
 import DetailProductPage from "./pages/detailProduct";
 import Products from "./components/products";
+
 router.on("/",() => printf('#app',HomePage.render(books)))
 router.on('/book/detail/:id',(data)=>printf('#app',DetailProductPage.render(books,data.data.id)))
 router.resolve();
